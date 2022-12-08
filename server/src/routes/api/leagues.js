8ba7e68b24c44b37411095df93853e7923296c3b
Wojciech Sutkowski <wojciechsutkowskii@gmail.com>
2022-12-08 21:35:18 +0100

    tmp

--- server/src/routes/api/leagues.js
+++ server/src/routes/api/leagues.js
@@ -29,6 +29,27 @@ const getLeagueById = async (leagueId) => {
   }
 };
 
+const getLeaguesByCountry = async (countryName) => {
+  try {
+    const leagues = await axios.get(
+      "https://v3.football.api-sports.io/leagues?country",
+      {
+        params: {
+          country: countryName,
+        },
+        headers: {
+          "x-rapidapi-key": process.env.API_KEY,
+          "x-rapidapi-host": "v3.football.api-sports.io",
+        },
+      }
+    );
+    console.log(leagues.data.response);
+    return leagues.data.response;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const getLeagueSeason = async (leagueId, leagueSeason) => {
   try {
     const season = await axios.get(
@@ -54,6 +75,11 @@ const getLeagueSeason = async (leagueId, leagueSeason) => {
 router.get("/:id", async (req, res) => {
   console.log(getLeagueById(req.params.id));
   res.send(await getLeagueById(req.params.id));
+});
+
+router.get("/country/:country", async (req, res) => {
+  console.log(getLeaguesByCountry(req.params.country));
+  res.send(await getLeaguesByCountry(req.params.country));
 });
 
 router.get("/:id/:season", async (req, res) => {
